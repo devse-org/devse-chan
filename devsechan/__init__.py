ff02ed3e84d577d7cd6990dcb143d289d7bcc0f8
https://supercip971@github.com <supercyp@protonmail.com>
2021-07-03 13:02:29 -0400

    refactor: better channel handling  with the config

--- devsechan/__init__.py
+++ devsechan/__init__.py
@@ -3,6 +3,7 @@ import confuse
 import re
 from devsechan.irc import IRC
 from devsechan.discord import Discord
+import discord as ds
 
 
 class DevSEChan:
@@ -13,30 +14,35 @@ class DevSEChan:
         self.irc = IRC(self, self.config['irc'])
         self.discord = Discord(self, self.config['discord'])
 
-    def get_member_ping_to_id(self, nickname, guild):
-        member = guild.get_member_named(nickname)
+    def get_guild(self):
+        return self.discord.get_guild(self.config['discord']['guild'].get())
+
+    def get_member_ping_to_id(self, nickname):
+        member = self.get_guild().get_member_named(nickname)
         if member is not None:
             return f"<@{member.id}>"
         return None
 
-    def convert_irc_mentions_to_discord(self, message, guild):
+    def get_guild_channel(self, name):
+        for channel in self.get_guild().channels:
+            if(channel.name == name):
+                return channel
+
+    def convert_irc_mentions_to_discord(self, message):
         mentions_regex = r"(?<=@)[a-zA-Z0-9]*"
         mentions_match = re.finditer(mentions_regex, message, re.MULTILINE)
 
         for mention in mentions_match:
             nickname = mention.group()
-            member_id = self.get_member_ping_to_id(nickname, guild)
+            member_id = self.get_member_ping_to_id(nickname)
             if member_id is not None:
                 message = message.replace('@' + nickname, member_id)
 
         return message
 
     async def to_discord(self, nick, message):
-        channel_id = self.config['discord']['channel'].get()
-        channel = self.discord.get_channel(channel_id)
-        guild = channel.guild
-        converted_message = self.convert_irc_mentions_to_discord(
-            message, guild)
+        channel = self.get_guild_channel(self.config['discord']['irc_channel'].get())
+        converted_message = self.convert_irc_mentions_to_discord( message)
         await channel.send(f"**<{nick}>** {converted_message}")
 
     def to_irc(self, author, msg_list):
--- devsechan/discord.py
+++ devsechan/discord.py
@@ -7,22 +7,33 @@ def Discord(parent, config):
     intents.members = True
     bot = discord.Client(intents=intents)
 
+    def get_guild():
+        return bot.get_guild(config['guild'].get())
+
+    def get_guild_channel(name):
+        return discord.utils.get(get_guild().channels, name=name)
+
     @bot.event
     async def on_message(message):
         if message.author == bot.user:
             return
-        if message.channel.id != config['channel'].get():
+        if message.channel.id != get_guild_channel(config['irc_channel'].get()).id:
             return
+        
         content = message.content
+
         for user in message.mentions:
             substitute = f"@{user.name}#{user.discriminator}"
             content = content.replace(f"<@!{user.id}>", substitute).replace(f"<@{user.id}>", substitute)
+        
         for channel in message.channel_mentions:
             content = content.replace(f"<#{channel.id}>", f"#{channel.name}")
+            
         msg_list = content.split('\n')
         attachments_count = len(message.attachments)
         for i in range(attachments_count):
             msg_list.append(message.attachments[i].url)
+            
         parent.to_irc(message.author, msg_list)
 
     return bot
